7b1aaca3fb5fc2cb41c01c47b53b6606c1b5134b
Sammy99jsp <sammy99jsp@gmail.com>
2024-01-28 19:18:27 +0000

    Add utility debug functions for `LexResult`

--- src/lexing/utils/result.rs
+++ src/lexing/utils/result.rs
@@ -48,3 +48,39 @@ pub enum LexResult<L> {
     ///
     Nothing,
 }
+
+impl<L> LexResult<L> {
+    ///
+    /// Allegory of [Result::expect]
+    ///
+    pub fn expect(self, msg: impl ToString) -> L {
+        match self {
+            LexResult::Lexed(lexed) => lexed,
+            LexResult::Errant(errant) => panic!("{}: {errant:?}", msg.to_string()),
+            LexResult::Nothing => panic!("{}: on LexResult::Nothing", msg.to_string()),
+        }
+    }
+
+    ///
+    /// Allegory of [Result::unwrap]
+    ///
+    pub fn unwrap(self) -> L {
+        match self {
+            LexResult::Lexed(lexed) => lexed,
+            LexResult::Errant(errant) => panic!("called `LexResult::unwrap()` on an `Errant` value: {errant:?}"),
+            LexResult::Nothing => panic!("called `LexResult::unwrap()` on a `Nothing` value"),
+        }
+    }
+
+    pub fn is_errant(&self) -> bool {
+        matches!(self, Self::Errant(_))
+    }
+
+    pub fn is_lexed(&self) -> bool {
+        matches!(self, Self::Lexed(_))
+    }
+
+    pub fn is_nothing(&self) -> bool {
+        matches!(self, Self::Nothing)
+    }
+}
